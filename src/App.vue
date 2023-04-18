a7e359c899ec644f9be5edace1744dbc920cd7e0
Moustapha <cmr.mous@gmail.com>
2023-04-18 15:06:30 +0200

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <main>
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="logo pinia" />
+      <h1>Pinia crash course - TNN</h1>
+    </header>
+  </main>
+</template>
+<style lang="scss" scoped></style>
