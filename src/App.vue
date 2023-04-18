b04815266a6e80abcc019092b970ff0aa9e6f737
Moustapha <cmr.mous@gmail.com>
2023-04-18 16:28:48 +0200

    v1

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,26 @@
       <img src="./assets/pinia-logo.svg" alt="logo pinia" />
       <h1>{{ taskStore.title }}</h1>
     </header>
-
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="setFilter('all')">All tasks</button>
+      <button @click="setFilter('favs')">Favourite tasks</button>
+      {{ filter }}
+    </nav>
     <!-- task list -->
 
-    <div class="task-list">
+    <!-- all tasks -->
+    <div v-if="filter === 'all'" class="task-list">
+      <p>All tasks</p>
       <div v-for="task in taskStore.tasks">
-        <p>
-          <TaskItem :task="task" />
-        </p>
+        <TaskItem :task="task" />
+      </div>
+    </div>
+    <!-- fav tasks -->
+    <div v-if="filter === 'favs'" class="task-list">
+      <p>Fav tasks</p>
+      <div v-for="task in taskStore.favs">
+        <TaskItem :task="task" />
       </div>
     </div>
   </main>
@@ -20,13 +32,20 @@
 <script>
 import { useTaskStore } from "./stores/TaskStore";
 import TaskItem from "./components/TaskItem.vue";
+import { ref } from "vue";
 
 export default {
   components: { TaskItem },
   setup() {
     const taskStore = useTaskStore();
 
-    return { taskStore };
+    const filter = ref("all");
+    return { taskStore, filter };
+  },
+  methods: {
+    setFilter(filter) {
+      this.filter = filter;
+    },
   },
 };
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -3,9 +3,19 @@ import { defineStore } from "pinia";
 export const useTaskStore = defineStore("taskStore", {
   state: () => ({
     tasks: [
-      { id: 1, title: "do the dishes", isFav: "false" },
-      { id: 2, title: "play Mario Kart", isFav: "true" },
+      { id: 1, title: "do the dishes", isFav: false },
+      { id: 2, title: "play Mario Kart", isFav: true },
     ],
     title: "Pinia crash course - TNN",
   }),
+
+  getters: {
+    favs() {
+      // this here refers to state
+
+      return this.tasks.filter((t) => t.isFav);
+      // it doesn't alter the array, it returns a new array based on the first one
+      // filter(t => t.isFav) = returns true if true, false if f
+    },
+  },
 });
